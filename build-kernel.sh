d0316d0e8cc8cfc5dc077e8eaca5e430e22b84f3
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2023-04-22 20:01:12 -0400

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -229,22 +229,27 @@ fi
 
 set_output elapsed_time "$(echo "$(date +%s)"-"$start_time" | bc)"
 msg "Packaging the kernel..."
-zip_filename="${name}-${tag}-${date}.zip"
-END
-touch Image.gz
-ls
-msg "mkdir..."
-mkdir out33
-mkdir -p out2/arch/arm64/boot
 
-cd out2/arch/arm64/boot/
-touch Image.gz
-ls
-cd
-ls
-    msg "No zip template provided, releasing the kernel image instead"
-    #set_output outfile /github/workspace/out2/arch/arm64/boot/Image.gz
-   set_output outfile Image.gz
+tools/mkbootimg --kernel out2/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --base 0x00478000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x0ff88000 --second_offset 0x00e88000 --tags_offset 0x07988000 --os_version 9.0.0 --os_patch_level 2020-01 --header_version 0 --output Kernel-4.9.319-enforcing-v1.img
+
+set_output outfile Kernel-4.9.319-enforcing-v1.img
+
+#zip_filename="${name}-${tag}-${date}.zip"
+END
+#touch Image.gz
+#ls
+#msg "mkdir..."
+#mkdir out33
+#mkdir -p out2/arch/arm64/boot
+
+#cd out2/arch/arm64/boot/
+#touch Image.gz
+#ls
+#cd
+#ls
+ #   msg "No zip template provided, releasing the kernel image instead"
+  #  #set_output outfile /github/workspace/out2/arch/arm64/boot/Image.gz
+  # set_output outfile Image.gz
 
     exit 0
 
